b18233cf90c35b06827354485509ce8a191fe8b5
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2023-11-06 11:39:32 +0200

    x3

--- VldTenshi/Secondpage.xaml.cs
+++ VldTenshi/Secondpage.xaml.cs
@@ -12,9 +12,55 @@ namespace VldTenshi
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Secondpage : ContentPage
     {
+        Label label;
         public Secondpage()
         {
-            
+            Editor editor= new Editor
+            {
+                Placeholder="Put the text here",
+                BackgroundColor= Color.DeepSkyBlue,
+                TextColor= Color.RosyBrown,
+            };
+            editor.TextChanged += Editor_TextChanged;
+            label = new Label
+            {
+                Text="MainTitle",
+                HorizontalOptions =LayoutOptions.Start,
+                VerticalOptions = LayoutOptions.Center,
+                TextColor = Color.SandyBrown,
+                BackgroundColor = Color.DeepSkyBlue,
+            };
+            Button b = new Button
+            {
+                Text = "To Start Page",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center,
+                BackgroundColor= Color.SandyBrown,
+            };
+            b.Clicked += B_Clicked;  
+            StackLayout st = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                Children = { label, editor,b },
+                BackgroundColor = Color.AliceBlue,
+            };
+            Content=st;
+            //st.Children.Add(b);
+        }
+        int i = 0;
+        private void Editor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            char key=e.NewTextValue?.LastOrDefault() ?? ' ';
+            if (key == 'A')
+            {
+                i++;
+                label.Text=key.ToString()+": "+i.ToString();
+            }
+        }
+
+        private async void B_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
         }
     }
 }
